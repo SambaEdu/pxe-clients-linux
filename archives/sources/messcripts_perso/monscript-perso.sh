102f7eda1118c62d512787d113d66abda433ad6b
msuquet <michel-emi.suquet@ac-versailles.fr>
2016-01-10 20:20:36 +0100

    script perso

--- archives/sources/messcripts_perso/monscript-perso.sh
+++ archives/sources/messcripts_perso/monscript-perso.sh
@@ -1,2 +1,29 @@
+#!/bin/bash
 
+#####
+#
+# Ci-dessous, vous pouvez ajouter des commandes
+# ces commandes seront prises en compte à la fin de la post-installation.
+#
+#
+
+ladate=$(date +%Y%m%d%H%M%S)
+
+#####
+# quelques couleurs ;-)
+#
+rouge='\e[0;31m'
+rose='\e[1;31m'
+orange='\e[0;33m'
+jaune='\e[1;33m'
+vert='\e[0;32m'
+bleu='\e[1;34m'
+neutre='\e[0;m'
+
+
+# ne laissez pas vide ce script : laissez au minimum la commande suivante :
+true
+
+#####
+# Vos commandes perso :
 
