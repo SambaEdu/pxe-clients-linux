948b58280df57dc7c833c3f9e3c1f0ebb0e3b6a9
msuquet <michel-emi.suquet@ac-versailles.fr>
2016-01-10 23:52:17 +0100

    script perso fonctions

--- archives/sources/messcripts_perso/monscript-perso.sh
+++ archives/sources/messcripts_perso/monscript-perso.sh
@@ -3,9 +3,23 @@
 #####
 #
 # Ci-dessous, vous pouvez ajouter des commandes
+# (à la fin du fichier)
 # ces commandes seront prises en compte à la fin de la post-installation.
 #
+# Des fonctions déjà disponibles vous donnent accès à des variables
+# vous pouvez utiliser dans ce script les variables suivantes :
+# → Paramètres Proxy :
+#               $ip_proxy
+#               $port_proxy
+# → Paramètres SE3 :
+#               $ip_se3
+#               $nom_se3
+#               $nom_domaine
+# → Paramètres LDAP :
+#               $ip_ldap
+#               $ldap_base_dn
 #
+
 
 ladate=$(date +%Y%m%d%H%M%S)
 
@@ -20,10 +34,45 @@ vert='\e[0;32m'
 bleu='\e[1;34m'
 neutre='\e[0;m'
 
+#----- -----
+# les fonctions (début)
+#----- -----
 
-# ne laissez pas vide ce script : laissez au minimum la commande suivante :
-true
+recuperer_parametres()
+{
+    # le fichier des paramètres a été mis en place à la fin de l'installation
+    . /root/bin/params.sh
+}
+
+test_se3()
+{
+    if [ -n "${ip_se3}" ]
+    then
+        TEST_CLIENT=$(ifconfig | grep ":$ip_se3 ")
+        if [ -e /var/www/se3 ]
+        then
+            echo -e "${rouge}Malheureux… Ce script est à exécuter sur les clients Linux, pas sur le serveur !${neutre}"
+            echo ""
+            exit 1
+        fi
+    else
+        echo -e "${rouge}IP se3 non trouvé…${neutre}"
+        echo ""
+        exit 1
+    fi
+}
+
+#----- -----
+# les fonctions (fin)
+#----- -----
 
 #####
+# début du programme
+recuperer_parametres
+test_se3
 # Vos commandes perso :
 
+
+exit 0
+# fin du programme
+#####
