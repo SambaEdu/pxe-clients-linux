d71f58a94263f90f80e2681593e84306168a5145
naldegheri <nicolas.aldegheri@ac-versailles.fr>
2016-12-03 20:38:51 +0100

    install v0.9 of apt-cacher-ng, from wheezy-backports-sloppy to avoid xenial installation/integration problems ...

--- src/install_client_linux_mise_en_place.sh
+++ src/install_client_linux_mise_en_place.sh
@@ -857,13 +857,13 @@ END
                 echo "VfilePattern = (^|.*/)(Index|Packages(\.gz|\.bz2|\.lzma|\.xz)?|InRelease|Release|Release\.gpg|Sources(\.gz|\.bz2|\.lzma|\.xz)?|release|index\.db-.*\.gz|Contents-[^/]*(\.gz|\.bz2|\.lzma|\.xz)?|pkglist[^/]*\.bz2|rclist[^/]*\.bz2|meta-release[^/]*|Translation[^/]*(\.gz|\.bz2|\.lzma|\.xz)?|MD5SUMS|SHA1SUMS|((setup|setup-legacy)(\.ini|\.bz2|\.hint)(\.sig)?)|mirrors\.lst|repo(index|md)\.xml(\.asc|\.key)?|directory\.yast|products|content(\.asc|\.key)?|media|filelists\.xml\.gz|filelists\.sqlite\.bz2|repomd\.xml|packages\.[a-zA-Z][a-zA-Z]\.gz|info\.txt|license\.tar\.gz|license\.zip|.*\.(db|files|abs)(\.tar(\.gz|\.bz2|\.lzma|\.xz))?|metalink\?repo|.*prestodelta\.xml\.gz|repodata/.*\.(xml|sqlite)(\.gz|\.bz2|\.lzma|\.xz))$|/dists/.*/installer-[^/]+/[^0-9][^/]+/images/.*"  >> /etc/apt-cacher-ng/acng.conf
             fi
         fi
-        # sous wheezy se3, installation de la version 0.8 d'apt-cacher-ng (backports de wheezy) pour prise en charge clients xenial
+        # sous wheezy se3, installation de la version 0.9 d'apt-cacher-ng (backports-sloppy de wheezy) pour prise en charge correct des clients xenial
         #apt_cacher_ng_version="$(dpkg-query -W apt-cacher-ng |cut -f2 - |cut -c1-3 -)"
         eval apt_cacher_ng_version=$(dpkg-query -W apt-cacher-ng | cut -c15-17 -)
-        if [ "$version_se3" = "wheezy" ] && [ "$apt_cacher_ng_version" != "0.8" ]
+        if [ "$version_se3" = "wheezy" ] && [ "$apt_cacher_ng_version" != "0.9" ]
         then
 			# On ajoute le depot backports de wheezy à la liste des depots du se3
-			echo 'deb http://ftp.fr.debian.org/debian/ wheezy-backports main' >> /etc/apt/sources.list
+			echo 'deb http://ftp.fr.debian.org/debian/ wheezy-backports-sloppy main' >> /etc/apt/sources.list
 			apt-get update -q2
 			# On pre-configure apt-cacher-ng pour qu'il garde sa configuration déjà existante à chaque maj
 debconf-set-selections << EOF
@@ -873,10 +873,10 @@ apt-cacher-ng	apt-cacher-ng/port	string	keep
 apt-cacher-ng	apt-cacher-ng/proxy	string	keep
 apt-cacher-ng	apt-cacher-ng/gentargetmode	select	No automated setup
 EOF
-			echo "Le se3 est wheezy : on installe la version 0.8 (backports) du paquet apt-cacher-ng" | tee -a $compte_rendu
-			DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get install -t wheezy-backports -q2 -y -o Dpkg::Options::="--force-confold" apt-cacher-ng
+			echo "Le se3 est wheezy : on installe la version 0.9 (du dépot backports-sloppy) du paquet apt-cacher-ng" | tee -a $compte_rendu
+			DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get install -t wheezy-backports-sloppy -q2 -y -o Dpkg::Options::="--force-confold" apt-cacher-ng
 			# Retrait des backports de la liste des dépots du se3 puis maj de la liste des paquets
-			sed -i "/^deb http:\/\/ftp.fr.debian.org\/debian\/ wheezy-backports main$/d" /etc/apt/sources.list
+			sed -i "/^deb http:\/\/ftp.fr.debian.org\/debian\/ wheezy-backports-sloppy main$/d" /etc/apt/sources.list
 			apt-get update -q2
         fi
         # redémarrage du serveur apt-cacher-ng
